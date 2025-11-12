3e1a0fddae03f330eb51988e1d6f090b09d5d7d2
Alexaldi <gilangaldiano05@gmail.com>
2025-11-12 22:58:06 +0700

    report controller fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -22,7 +22,7 @@ class ReportController extends Controller
         $journeys = $this->journeyService->paginateByReport($report->id, 5, order: 'desc');
 
         $institutions = Institution::orderBy('name')->get(['id', 'name']);
-        $divisions = Division::orderBy('name')->get(['id', 'name', 'institution_id']);
+        $divisions = Division::orderBy('name')->get(['id', 'name', 'type']);
 
         $journeyTypes = ReportJourneyType::manualOptions();
 
